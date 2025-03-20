749d7a43014920a9cfde8d9c30d48cc305c345e5
Kipkemoi Fred <fred.kipkemoi@tracom.co.ke>
2025-03-20 19:40:08 +0300

    initial commit

---
+++ src/main/java/com/example/Loan_Management_Module/LoanManagementModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Loan_Management_Module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanManagementModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanManagementModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Loan_Management_Module/LoanManagementModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Loan_Management_Module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
